bf53636c704550035f82ea061abe072d4b784d40
Nevov21 <jaceszyk@gmail.com>
2024-11-03 14:22:35 +0100

    set rnu numbers orange

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,6 +5,9 @@ vim.cmd("set shiftwidth=2")
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
 
-vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#FFA500' })
 vim.o.guifont = "Hack Nerd Font:h17"
 
+-- Set relative line number as orange (do this after loading theme)
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "grey" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "orange" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "grey" })
